7f1b1ff9cfc3c77244f24bcb1fc4b40212d267d9
kalozfauzan <kaloz.fauzan2@gmail.com>
2022-10-15 14:16:06 +0700

    add ourlife page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/our-life', function(req, res, next) {
+  res.render('our-life', { title: 'Express' });
+});
+
 module.exports = router;
