e0ce91b50fb44d650ae1c78b49b116331f135cc2
kalozfauzan <kaloz.fauzan2@gmail.com>
2022-10-16 01:02:16 +0700

    add promotion page and ourlife article

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,15 @@ router.get('/our-life', function(req, res, next) {
   res.render('our-life', { title: 'Express' });
 });
 
+router.get('/our-life-article', function(req, res, next) {
+  res.render('our-life-article', { title: 'Express' });
+});
+
+router.get('/promotion-event', function(req, res, next) {
+  res.render('promotion-event', { title: 'Express' });
+});
+router.get('/promotion-magazine', function(req, res, next) {
+  res.render('promotion-magazine', { title: 'Express' });
+});
+
 module.exports = router;
